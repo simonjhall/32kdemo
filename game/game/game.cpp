ee1c88bcc4e993d2cb58ecdf59fe3549ac2b9916
Simon Hall <simonjhall@gmail.com>
2017-02-10 17:23:09 +0000

    update for input, plot on actual 68k some fonts

--- game/game/game.cpp
+++ game/game/game.cpp
@@ -17,6 +17,7 @@ static const short s_scaleFactorY = 1;
 static CHAR_INFO s_consoleBuffer[(s_width * s_scaleFactorX) * (s_height * s_scaleFactorY)];
 static CHAR_INFO s_lastConsoleBuffer[(s_width * s_scaleFactorX) * (s_height * s_scaleFactorY)];
 
+#if defined _WIN32
 void plot_pixel(short x, short y, short col)
 {
 	x = x * s_scaleFactorX;
@@ -30,6 +31,31 @@ void plot_pixel(short x, short y, short col)
 	//http://benryves.com/tutorials/winconsole/4
 	s_consoleBuffer[(s_width * s_scaleFactorX) * y + x].Attributes = col ? (BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | BACKGROUND_INTENSITY) : 0;
 }
+#elif defined __m68k__
+void plot(long x, long y, short col)
+{
+	unsigned int white = 0xffffff;
+	/*volatile unsigned short *pRam = (volatile unsigned short *)(513 * 1024);
+	pRam[y * 256 + x] = 0xff;*/
+	__asm__ __volatile__
+	(
+		//pen colour
+		"move.l %0, %%d1\n\t"
+		"move.b #80, %%d0\n\t"
+		"trap #15\n\t"
+
+		//plot pixel
+		"move.l %1, %%d1\n\t"
+		"move.l %2, %%d2\n\t"
+		"move.b #82, %%d0\n\t"
+		"trap #15\n\t"
+
+		: /*outputs*/
+	: "g" (white), "g" (x), "g" (y) /*inputs*/
+		: "d0", "d1", "d2" /*clobbers*/
+		);
+}
+#endif
 
 void draw_square(short x_pos, short y_pos, short width, short height, short col)
 {
@@ -93,20 +119,54 @@ int main()
 	memset(&s_lastConsoleBuffer, sizeof(s_lastConsoleBuffer), 0x7f);
 	clear_screen(0);
 
-	float time = 0;
+	short x = 128 - 25;
+	short y = 128 - 25;
 
 	while (1)
 	{
+		DWORD numEvents, numEventsRead;
+		ok = GetNumberOfConsoleInputEvents(rHnd, &numEvents);
+		assert(ok == TRUE);
+
+		if (numEvents != 0)
+		{
+			INPUT_RECORD *eventBuffer = new INPUT_RECORD[numEvents];
+			ReadConsoleInput(rHnd, eventBuffer, numEvents, &numEventsRead);
+
+			for (DWORD count = 0; count < numEventsRead; count++)
+			{
+				if (eventBuffer[count].EventType == KEY_EVENT)
+				{
+					switch (eventBuffer[count].Event.KeyEvent.wVirtualKeyCode)
+					{
+					case VK_ESCAPE:
+						exit(0);
+					case VK_LEFT:
+						x--;
+						break;
+					case VK_RIGHT:
+						x++;
+						break;
+					case VK_UP:
+						y--;
+						break;
+					case VK_DOWN:
+						y++;
+						break;
+					default:
+						break;
+					}
+				}
+			}
+
+			delete[] eventBuffer;
+		}
+
 		clear_screen(0);
 
-		short x = short(sin(time) * 100);
-		short y = short(cos(time) * 100);
-
-		draw_square(128 + x - 25, 128 + y - 25, 50, 50, 1);
+		draw_square(x, y, 50, 50, 1);
 
 		paint_screen();
-
-		time += 0.1f;
 	}
 
 
